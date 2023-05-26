746a3a98e5aac5313d3fd86e229af538a37e4a57
keixjk <kayegallogo@gmail.com>
2023-05-26 21:48:43 +0800

    main math operations

---
+++ OOPcalculator.py
@@ -0,0 +1,15 @@
+# calculator
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    # main math operations
+    def add (self, num1, num2):
+        return num1 + num2
+    def subtract (self, num1, num2):
+        return num1 - num2
+    def multiply (self, num1, num2):
+        return num1 * num2
+    def divide (self, num1, num2):
+        return num1 / num2
